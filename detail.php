24848a07b146b6c37abfaaeec1ed9d7647212f0e
Fraina <jscaem@gmail.com>
2016-09-13 17:17:52 +0800

    refactor(detail.php): img detail page, close #4

--- detail.php
+++ detail.php
@@ -11,6 +11,7 @@
 if (!defined('DOKU_INC')) die();
 header('X-UA-Compatible: IE=edge,chrome=1');
 
+$showSidebar = page_findnearest($conf['sidebar']) && ($ACT=='show');
 ?><!DOCTYPE html>
 <html lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction'] ?>" class="no-js">
 <head>
@@ -27,84 +28,95 @@ header('X-UA-Compatible: IE=edge,chrome=1');
 </head>
 
 <body>
-    <!--[if lte IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
-    <div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?>">
-
+    <div class="wrapper">
+      <script>console.log(<?php
+        echo json_encode($conf['sidebar']);
+      ?>);</script>
+        <?php
+            // render the content into buffer for later use
+            ob_start();
+            tpl_content(false);
+            $buffer = ob_get_clean();
+        ?>
         <?php include('tpl_header.php') ?>
 
-        <div class="wrapper group" id="dokuwiki__detail">
+        <div class="wrapper-content">
+            <div class="sidebar fl">
+                <aside class="sidebar-aside">
+                    <?php if(!$ERROR): ?>
+                      <h3 class="sidebar-title"><?php echo nl2br(hsc(tpl_img_getTag('simple.title'))); ?></h3>
+                    <?php endif; ?>
 
-            <!-- ********** CONTENT ********** -->
-            <div id="dokuwiki__content"><div class="pad group">
-
-                <?php if(!$ERROR): ?>
-                    <div class="pageId"><span><?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG)); ?></span></div>
-                <?php endif; ?>
-
-                <div class="page group">
+                    <div class="sidebar-breadcrumbs">
+                        <?php if($conf['breadcrumbs'] || $conf['youarehere']): ?>
+                            <div class="breadcrumbs">
+                                <?php if($conf['youarehere']): ?>
+                                    <div class="youarehere"><?php tpl_youarehere() ?></div>
+                                <?php endif ?>
+                                <?php if($conf['breadcrumbs']): ?>
+                                    <div class="trace"><?php tpl_breadcrumbs() ?></div>
+                                <?php endif ?>
+                            </div>
+                        <?php endif ?>
+                    </div>
+                    <div class="sidebar-page">
+                      <?php if ($showSidebar): ?>
+                        <?php tpl_includeFile('sidebarheader.html') ?>
+                        <?php tpl_include_page($conf['sidebar'], 1, 1) /* includes the nearest sidebar page */ ?>
+                        <?php tpl_includeFile('sidebarfooter.html') ?>
+                      <?php endif; ?>
+                    </div>
+                </aside>
+            </div>
+            <div class="main fr">
+                <div class="detail-wrapper">
                     <?php tpl_flush() ?>
-                    <?php tpl_includeFile('pageheader.html') ?>
-                    <!-- detail start -->
                     <?php
                     if($ERROR):
                         echo '<h1>'.$ERROR.'</h1>';
                     else: ?>
                         <?php if($REV) echo p_locale_xhtml('showrev');?>
-                        <h1><?php echo nl2br(hsc(tpl_img_getTag('simple.title'))); ?></h1>
+                        <h2><?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG)); ?></h2>
 
-                        <?php tpl_img(900,700); /* parameters: maximum width, maximum height (and more) */ ?>
+                        <div class="detail-img">
+                          <?php tpl_img(); /* parameters: maximum width, maximum height (and more) */ ?>
+                        </div>
 
-                        <div class="img_detail">
+                        <div class="detail-meta">
                             <?php tpl_img_meta(); ?>
                         </div>
                         <?php //Comment in for Debug// dbg(tpl_img_getTag('Simple.Raw'));?>
                     <?php endif; ?>
                 </div>
-                <!-- detail stop -->
-                <?php tpl_includeFile('pagefooter.html') ?>
-                <?php tpl_flush() ?>
 
-                <?php /* doesn't make sense like this; @todo: maybe add tpl_imginfo()?
-                <div class="docInfo"><?php tpl_pageinfo(); ?></div>
-                */ ?>
+                <!-- PAGE ACTIONS -->
+                <?php if (!$ERROR): ?>
+                    <ul class="detail-tools">
+                        <?php
+                            $data = array(
+                                'view' => 'detail',
+                                'items' => array(
+                                    'mediaManager' => tpl_action('mediaManager', 1, 'li', 1),
+                                    'img_backto' =>   tpl_action('img_backto', 1, 'li', 1)
+                                )
+                            );
 
-            </div></div><!-- /content -->
+                            // the page tools can be amended through a custom plugin hook
+                            $evt = new Doku_Event('TEMPLATE_PAGETOOLS_DISPLAY', $data);
+                            if($evt->advise_before()) {
+                                foreach($evt->data['items'] as $k => $html) echo $html;
+                            }
+                            $evt->advise_after();
+                            unset($data);
+                            unset($evt);
+                        ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 
-            <hr class="a11y" />
-
-            <!-- PAGE ACTIONS -->
-            <?php if (!$ERROR): ?>
-                <div id="dokuwiki__pagetools">
-                    <h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
-                    <div class="tools">
-                        <ul>
-                            <?php
-                                $data = array(
-                                    'view' => 'detail',
-                                    'items' => array(
-                                        'mediaManager' => tpl_action('mediaManager', 1, 'li', 1, '<span>', '</span>'),
-                                        'img_backto' =>   tpl_action('img_backto',   1, 'li', 1, '<span>', '</span>'),
-                                    )
-                                );
-
-                                // the page tools can be amended through a custom plugin hook
-                                $evt = new Doku_Event('TEMPLATE_PAGETOOLS_DISPLAY', $data);
-                                if($evt->advise_before()) {
-                                    foreach($evt->data['items'] as $k => $html) echo $html;
-                                }
-                                $evt->advise_after();
-                                unset($data);
-                                unset($evt);
-                            ?>
-                        </ul>
-                    </div>
-                </div>
-            <?php endif; ?>
-        </div><!-- /wrapper -->
-
-        <?php include('tpl_footer.php') ?>
-    </div></div><!-- /site -->
-
+    <?php include('tpl_footer.php') ?>
     <!--[if ( lte IE 7 | IE 8 ) ]></div><![endif]-->
 </body>
 </html>
